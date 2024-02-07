90e9fd2446fce610f21c9074f9724afaf8af61db
mike-dalton <michael.j.dalton@gmail.com>
2024-02-07 17:52:43 -0500

    Finish setCardArray helper

--- app/helpers/setCardArray.js
+++ app/helpers/setCardArray.js
@@ -1,18 +1,18 @@
-import { convertCircuitToInteger, convertIntegerToCircuit } from "../helpers/convertCircuit";
-
-/**
- * We need to return an array containing the number of tasks demanded,
- * plus any empty tasks to ensure that the card is square.
- *
- * First we'll add all of the elements from the settings into the cardArray.
- * If we have too many elements, we'll randomize the order and cut the array.
- * If we need extra elements, we'll add more climbs first, then duplicate colours,
- * exercises & lastly intros.
- * Next, we'll add any empty tasks that we need to make up the numbers.
- * Lastly, we'll randomize the order of the array and return it.
- */
 const randomInteger = function(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+// Map where to put the empty spaces for a given number of tasks demanded
+const setEmptiesArray = function(nbTasks) {
+  if (nbTasks === 8) {
+    return [1, 2, 5, 7, 8, 10, 13, 14];
+  }
+
+  if (nbTasks === 12) {
+    return [2, 7, 8, 13];
+  }
+
+  return [];
 };
 
 /**
@@ -20,62 +20,63 @@ const randomInteger = function(min, max) {
  * plus any empty tasks to ensure that the card is square.
  *
  * First we'll add all of the elements from the settings into the cardArray.
- * If we have too many elements, we'll randomize the order and cut the array.
- * If we need extra elements, we'll add more climbs first, then duplicate colours,
- * exercises & lastly intros.
- * Next, we'll randomize the order of the array
- * Lastly, we'll add any empty tasks that we need to make up the numbers, and return it.
+ * If we need extra elements, we'll add more climbs.
+ *
+ * Next, we'll randomize the order of the array.
+ * If we have too many elements, we'll cut the array.
+ *
+ * Lastly, we'll add any empty tasks that we need to fill the grid, and return it.
  */
 const setCardArray = function(nbTasks, colours, intros, exercises, climbRange) {
 
-  const cardArray = [];
+  const taskArray = [];
 
   if (colours.length > 0) {
-    cardArray.push(...colours);
+    taskArray.push(...colours);
   }
 
   if (intros) {
-    cardArray.push({ content: 'All the intros', type: 'intros' });
+    taskArray.push({ content: 'All the intros', type: 'intros' });
   }
 
   if (exercises.length > 0) {
-    cardArray.push(...exercises);
+    taskArray.push(...exercises);
   }
-  console.log('cardArray.length b4 climbs', cardArray.length);
 
   /////////////////////////////////////////////////////////////////
   // Maybe we need to use a SET to make sure no duplicate climbs?
   /////////////////////////////////////////////////////////////////
 
-  // If the card is already full, add 5 climbs by default
-  if (climbRange.length > 0 && cardArray.length >= nbTasks) {
+  // If we already have enough tasks, add 5 climbs by default
+  if (taskArray.length >= nbTasks) {
     for (let i = 0; i < 5; i++) {
       const difficulty = randomInteger(climbRange[0], climbRange[1]);
-      cardArray.push({content: difficulty, type: 'climb'});
+      taskArray.push({ content: difficulty, type: 'climb' });
     }
   // else fill the remaining tasks with climbs
-  } else if (climbRange.length > 0 && cardArray.length < nbTasks) {
-    while (cardArray.length < nbTasks) {
+  } else {
+    while (taskArray.length < nbTasks) {
       const difficulty = randomInteger(climbRange[0], climbRange[1]);
-      cardArray.push({content: difficulty, type: 'climb'});
+      taskArray.push({ content: difficulty, type: 'climb' });
     }
   }
-  console.log('cardArray.length after', cardArray.length);
 
+  // Shuffle the padded array using Fisher-Yates
+  for (let i = taskArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [taskArray[i], taskArray[j]] = [taskArray[j], taskArray[i]];
+  }
+  
+  // Trim the taskArray if it's longer than the number of tasks required
+  const cardArray = taskArray.slice(0, nbTasks);
+
+  // If we need empties, add them
+  if (nbTasks < 16) {
+    const emptiesArray = setEmptiesArray(nbTasks);
+    emptiesArray.forEach(emptyIndex => cardArray.splice(emptyIndex, 0, { content: '', type: 'empty' }));
+  }
 
   return cardArray;
 };
 
-const nbRequestedTasks = 16;
-
-const testColours = ['blue', 'red', 'black', 'white', 'purple', 'yellow'];
-const testColoursShort = [];
-
-const testExercises = ['sit-ups', 'push-ups', 'squats'];
-const testExercisesLong = ['sit-ups', 'push-ups', 'squats', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-
-const testClimbRange = [4, 10];
-// const setCardArray = function(nbTasks, colours, intros, exercises, climbRange)
-const result = setCardArray(nbRequestedTasks, testColours, true, testExercises, testClimbRange);
-
-console.log(result);
+module.exports = { setCardArray };
